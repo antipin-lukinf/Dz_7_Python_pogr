e36f2b7b8f9ff8496ca7e6248a1bf36275f602ff
Drei4 <antipin-lukinf@mail.ru>
2023-06-29 11:52:22 +0300

    Kl_zd_1 complit

--- Klass_rab_7/zd_1.py
+++ Klass_rab_7/zd_1.py
@@ -13,7 +13,7 @@ my_text = list()
 strings_count = 0
 
 
-def read_file(strings_count=3, file_name='text_data.txt'):
+def read_file(strings_count=2, file_name='text_data.txt'):
     strings_count_1 = strings_count
     with open(file_name, 'a+', encoding='utf-8') as f:
         for i in range(strings_count_1):
